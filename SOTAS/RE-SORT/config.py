886fcd48bd4d1c3cee99d91cc9a824e10cadbf6d
RE-SORT <157569662+RE-SORT@users.noreply.github.com>
2024-02-20 13:39:04 +0800

    Add files via upload

---
+++ SOTAS/RE-SORT/config.py
@@ -0,0 +1,82 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+
+parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                    help='number of total epochs to run')
+
+
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--cos', '--cosine_lr', default=1, type=int,
+                    metavar='COS', help='lr decay by decay', dest='cos')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)',
+                    dest='weight_decay')
+parser.add_argument('--seed', default=2023, type=int,
+                    help='seed for initializing training. ')
+
+
+
+
+# for number of fourier spaces
+parser.add_argument ('--num_f', type=int, default=3, help = 'number of fourier spaces')
+
+parser.add_argument ('--sample_rate', type=float, default=1.0, help = 'sample ratio of the features involved in balancing')
+parser.add_argument ('--lrbl', type = float, default = 0.8, help = 'learning rate of balance')
+
+parser.add_argument ('--lambdap', type = float, default = 80.0, help = 'weight decay for weight1 ')
+parser.add_argument ('--lambdapre', type = float, default = 1, help = 'weight for pre_weight1 ')
+
+parser.add_argument ('--epochb', type = int, default = 6, help = 'number of epochs to balance')
+parser.add_argument ('--epochp', type = int, default = 0, help = 'number of epochs to pretrain')
+
+# parser.add_argument ('--n_feature', type=int, default=128, help = 'number of pre-saved features')
+# parser.add_argument ('--feature_dim', type=int, default=512, help = 'the dim of each feature')
+
+parser.add_argument ('--lrwarmup_epo', type=int, default=0, help = 'the dim of each feature')
+parser.add_argument ('--lrwarmup_decay', type=int, default=0.1, help = 'the dim of each feature')
+
+parser.add_argument ('--n_levels', type=int, default=1, help = 'number of global table levels')
+
+# for expectation
+parser.add_argument ('--lambda_decay_rate', type=float, default=0.9, help = 'ratio of epoch for lambda to decay')
+parser.add_argument ('--lambda_decay_epoch', type=int, default=5, help = 'number of epoch for lambda to decay')
+parser.add_argument ('--min_lambda_times', type=float, default=0.001, help = 'number of global table levels')
+
+# for jointly train
+parser.add_argument ('--train_cnn_with_lossb', type=bool, default=False, help = 'whether train cnn with lossb')
+parser.add_argument ('--cnn_lossb_lambda', type=float, default=0, help = 'lambda for lossb')
+
+# for more moments
+parser.add_argument ('--moments_lossb', type=float, default=1, help = 'number of moments')
+
+# for first step
+parser.add_argument ('--first_step_cons', type=float, default=1, help = 'constrain the weight at the first step')
+
+# for pow
+parser.add_argument ('--decay_pow', type=float, default=2, help = 'value of pow for weight decay')
+
+# for second order moment weight
+parser.add_argument ('--second_lambda', type=float, default=0.3, help = 'weight lambda for second order moment loss')
+parser.add_argument ('--third_lambda', type=float, default=0.05, help = 'weight lambda for second order moment loss')
+
+# for dat/.a aug
+parser.add_argument ('--lower_scale', type=float, default=0.8, help = 'weight lambda for second order moment loss')
+
+# for lr decay epochs
+parser.add_argument ('--epochs_decay', type=list, default=[24, 30], help = 'weight lambda for second order moment loss')
+
+parser.add_argument ('--classes_num', type=int, default=5, help = 'number of epoch for lambda to decay')
+
+parser.add_argument ('--dataset', type=str, default="PACS", help = '')
+parser.add_argument ('--sub_dataset', type=str, default="", help = '')
+parser.add_argument ('--gray_scale', type=float, default=0.1, help = 'weight lambda for second order moment loss')
+
+parser.add_argument('--sum', type=bool, default=True, help='sum or concat')
+parser.add_argument('--concat', type=int, default=1, help='sum or concat')
+parser.add_argument('--min_scale', type=float, default=0.8, help='')
+parser.add_argument('--presave_ratio', type=float, default=0.9, help='the ratio for presaving features')
+
